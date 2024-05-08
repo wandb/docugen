42133c8a8893300a78e89e3a8fb15a058054331a
Kyle Goyette <kyle@wandb.com>
2024-05-07 21:28:59 -0400

    modify config, remove paginator

--- library.py
+++ library.py
@@ -149,7 +149,6 @@ def configure_doc_hiding():
         wandb.data_types.WBValue,
         wandb.data_types.Media,
         wandb.data_types.BatchableMedia,
-        wandb.apis.public.Paginator,
     ]
 
     for cls in base_classes:
