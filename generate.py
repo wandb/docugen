c08ffeb3bc0bb89fe9f8636925a292947e506e35
Noah Luna <15202580+ngrayluna@users.noreply.github.com>
2025-01-21 11:48:59 -0800

    Removed commented code

--- generate.py
+++ generate.py
@@ -149,41 +149,6 @@ def single_folder_format(directory):
                 os.rmdir(root)
 
 
-# def reformat_and_rename_readme(directory, title_mapping):
-#     for root, _, files in os.walk(directory):
-#         for file in files:
-#             if file == "README.md" or file == "_index.md":
-#                 file_path = os.path.join(root, file)
-                
-#                 with open(file_path, "r", encoding="utf-8") as f:
-#                     lines = f.readlines()
-
-#                 # Process the lines
-#                 updated_lines = []
-#                 for line in lines:
-#                     # Remove CTA buttons
-#                     if re.match(r"\{\{\<\s*cta-button.*?\>\}\}", line.strip()):
-#                         continue
-                    
-#                     # Update the title
-#                     if line.startswith("title:"):
-#                         old_title = line[len("title:"):].strip()
-#                         # Check if the title is in the mapping
-#                         new_title = title_mapping.get(old_title, old_title.capitalize())
-#                         line = f"title: {new_title}\n"
-                    
-#                     updated_lines.append(line)
-
-#                 # Rename README.md to _index.md
-#                 new_file_path = os.path.join(root, "_index.md")
-#                 if file == "README.md":
-#                     os.rename(file_path, new_file_path)
-#                     file_path = new_file_path
-
-#                 # Write back the updated file
-#                 with open(file_path, "w", encoding="utf-8") as f:
-#                     f.writelines(updated_lines)
-#                 print(f"Processed: {file_path}")
 
 def reformat_and_rename_readme(directory, title_mapping):
     for root, _, files in os.walk(directory):
